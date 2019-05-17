6b64146d70c9a01b45893a3c30438f51392edefb
steampunklink <nethhorn@gmail.com>
2019-05-17 15:19:21 -0700

    added scripts

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
+
+const users = require("./routes/api/users");
+const series = require("./routes/api/series");
+const entries = require("./routes/api/entries");
+const messages = require("./routes/api/messages");
+const comments = require("./routes/api/comments");
+const tickets = require("./routes/api/tickets");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.use(passport.initialize());
+require("./config/passport.js")(passport);
+
+app.use("/api/users", users);
+app.use("/api/series", series);
+app.use("/api/entries", entries);
+app.use("/api/messages", messages);
+app.use("/api/comment", comments);
+app.use("/api/tickets", tickets);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Piranhas swimming on port ${port}`));
